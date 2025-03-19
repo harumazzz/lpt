eb59e46928027bc65f9be4e7ad5276ed2760e899
Haruma <114157573+Haruma-VN@users.noreply.github.com>
2025-03-20 00:45:32 +0700

    finished logic

--- lpt/lib/util/calendar/vietnam_calendar.dart
+++ lpt/lib/util/calendar/vietnam_calendar.dart
@@ -2,12 +2,19 @@ import 'dart:math' as math;
 
 class VietnamCalendar {
   static int jdFromDate(int dd, int mm, int yy) {
-    final a = ((14 - mm) / 12).toInt();
+    final a = ((14 - mm) / 12).floor();
     final y = yy + 4800 - a;
     final m = mm + 12 * a - 3;
-    var jd = (dd + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045).toInt();
+    var jd =
+        dd +
+        ((153 * m + 2) / 5).floor() +
+        365 * y +
+        (y / 4).floor() -
+        (y / 100).floor() +
+        (y / 400).floor() -
+        32045;
     if (jd < 2299161) {
-      jd = (dd + (153 * m + 2) / 5 + 365 * y + y / 4 - 32083).toInt();
+      jd = (dd + (153 * m + 2) / 5 + 365 * y + y / 4 - 32083).floor();
     }
     return jd;
   }
@@ -16,19 +23,19 @@ class VietnamCalendar {
     final (b, c) = (() {
       if (jd > 2299160) {
         var a = jd + 32044;
-        var b = ((4 * a + 3) / 146097).toInt();
-        var c = (a - (b * 146097) / 4).toInt();
+        var b = ((4 * a + 3) / 146097).floor();
+        var c = (a - (b * 146097) / 4).floor();
         return (b, c);
       } else {
         return (0, jd + 32082);
       }
     }());
-    final d = ((4 * c + 3) / 1461).toInt();
-    final e = (c - (1461 * d) / 4).toInt();
-    final m = ((5 * e + 2) / 153).toInt();
-    final day = (e - (153 * m + 2) / 5 + 1).toInt();
-    final month = (m + 3 - 12 * (m / 10)).toInt();
-    final year = (b * 100 + d - 4800 + m / 10).toInt();
+    final d = ((4 * c + 3) / 1461).floor();
+    final e = (c - (1461 * d) / 4).floor();
+    final m = ((5 * e + 2) / 153).floor();
+    final day = (e - (153 * m + 2) / 5 + 1).floor();
+    final month = (m + 3 - 12 * (m / 10)).floor();
+    final year = (b * 100 + d - 4800 + m / 10).floor();
     return (day, month, year);
   }
 
@@ -40,10 +47,14 @@ class VietnamCalendar {
     final T = (jdn - 2451545.0) / 36525;
     final t2 = T * T;
     final dr = math.pi / 180;
-    final M = 357.52910 + 35999.05030 * T - 0.0001559 * t2 - 0.00000048 * T * t2;
+    final M =
+        357.52910 + 35999.05030 * T - 0.0001559 * t2 - 0.00000048 * T * t2;
     final l0 = 280.46645 + 36000.76983 * T + 0.0003032 * t2;
     var dl = (1.914600 - 0.004817 * T - 0.000014 * t2) * math.sin(dr * M);
-    dl = dl + (0.019993 - 0.000101 * T) * math.sin(dr * 2 * M) + 0.000290 * math.sin(dr * 3 * M);
+    dl =
+        dl +
+        (0.019993 - 0.000101 * T) * math.sin(dr * 2 * M) +
+        0.000290 * math.sin(dr * 3 * M);
     var L = l0 + dl;
     L = L - 360 * (integerOf(L / 360));
     return L;
@@ -62,22 +73,38 @@ class VietnamCalendar {
     final t2 = T * T;
     final t3 = t2 * T;
     final dr = math.pi / 180;
-    var jd1 = 2415020.75933 + 29.53058868 * k + 0.0001178 * t2 - 0.000000155 * t3;
+    var jd1 =
+        2415020.75933 + 29.53058868 * k + 0.0001178 * t2 - 0.000000155 * t3;
     jd1 = jd1 + 0.00033 * math.sin((166.56 + 132.87 * T - 0.009173 * t2) * dr);
     final M = 359.2242 + 29.10535608 * k - 0.0000333 * t2 - 0.00000347 * t3;
     final mpr = 306.0253 + 385.81691806 * k + 0.0107306 * t2 + 0.00001236 * t3;
     final F = 21.2964 + 390.67050646 * k - 0.0016528 * t2 - 0.00000239 * t3;
-    var c1 = (0.1734 - 0.000393 * T) * math.sin(M * dr) + 0.0021 * math.sin(2 * dr * M);
+    var c1 =
+        (0.1734 - 0.000393 * T) * math.sin(M * dr) +
+        0.0021 * math.sin(2 * dr * M);
     c1 = c1 - 0.4068 * math.sin(mpr * dr) + 0.0161 * math.sin(dr * 2 * mpr);
     c1 = c1 - 0.0004 * math.sin(dr * 3 * mpr);
     c1 = c1 + 0.0104 * math.sin(dr * 2 * F) - 0.0051 * math.sin(dr * (M + mpr));
-    c1 = c1 - 0.0074 * math.sin(dr * (M - mpr)) + 0.0004 * math.sin(dr * (2 * F + M));
-    c1 = c1 - 0.0004 * math.sin(dr * (2 * F - M)) - 0.0006 * math.sin(dr * (2 * F + mpr));
-    c1 = c1 + 0.0010 * math.sin(dr * (2 * F - mpr)) + 0.0005 * math.sin(dr * (2 * mpr + M));
+    c1 =
+        c1 -
+        0.0074 * math.sin(dr * (M - mpr)) +
+        0.0004 * math.sin(dr * (2 * F + M));
+    c1 =
+        c1 -
+        0.0004 * math.sin(dr * (2 * F - M)) -
+        0.0006 * math.sin(dr * (2 * F + mpr));
+    c1 =
+        c1 +
+        0.0010 * math.sin(dr * (2 * F - mpr)) +
+        0.0005 * math.sin(dr * (2 * mpr + M));
     final deltat =
         (() {
           if (T < -11) {
-            return 0.001 + 0.000839 * T + 0.0002261 * t2 - 0.00000845 * t3 - 0.000000081 * T * t3;
+            return 0.001 +
+                0.000839 * T +
+                0.0002261 * t2 -
+                0.00000845 * t3 -
+                0.000000081 * T * t3;
           } else {
             return -0.000278 + 0.000265 * T + 0.000262 * t2;
           }
@@ -110,16 +137,25 @@ class VietnamCalendar {
     final k = integerOf(0.5 + (a11 - 2415021.076998695) / 29.530588853);
     var last = null as int?;
     var i = 1;
-    var arc = integerOf(getSunLongitude(getNewMoonDay(k + i, timeZone), timeZone) / 30);
+    var arc = integerOf(
+      getSunLongitude(getNewMoonDay(k + i, timeZone), timeZone) / 30,
+    );
     do {
       last = arc;
       i++;
-      arc = integerOf(getSunLongitude(getNewMoonDay(k + i, timeZone), timeZone) / 30);
+      arc = integerOf(
+        getSunLongitude(getNewMoonDay(k + i, timeZone), timeZone) / 30,
+      );
     } while (arc != last && i < 14);
     return i - 1;
   }
 
-  static (int, int, int, int) convertSolar2Lunar(int dd, int mm, int yy, double timeZone) {
+  static (int, int, int, int) convertSolar2Lunar(
+    int dd,
+    int mm,
+    int yy, [
+    double timeZone = 7.0,
+  ]) {
     var lunarDay = null as int?;
     var lunarMonth = null as int?;
     var lunarYear = null as int?;
@@ -170,9 +206,15 @@ class VietnamCalendar {
   ) {
     final (a11, b11) = (() {
       if (lunarMonth < 11) {
-        return (getLunarMonth11(lunarYear - 1, timeZone), getLunarMonth11(lunarYear, timeZone));
+        return (
+          getLunarMonth11(lunarYear - 1, timeZone),
+          getLunarMonth11(lunarYear, timeZone),
+        );
       } else {
-        return (getLunarMonth11(lunarYear, timeZone), getLunarMonth11(lunarYear + 1, timeZone));
+        return (
+          getLunarMonth11(lunarYear, timeZone),
+          getLunarMonth11(lunarYear + 1, timeZone),
+        );
       }
     }());
     final k = integerOf(0.5 + (a11 - 2415021.076998695) / 29.530588853);
@@ -194,5 +236,47 @@ class VietnamCalendar {
     }
     final monthStart = getNewMoonDay(k + off, timeZone);
     return jdToDate(monthStart + lunarDay - 1);
+  }
+
+  static String getCanChiYear(int lunarYear) {
+    const can = [
+      'Canh',
+      'Tân',
+      'Nhâm',
+      'Quý',
+      'Giáp',
+      'Ất',
+      'Bính',
+      'Đinh',
+      'Mậu',
+      'Kỷ',
+    ];
+    const chi = [
+      'Thân',
+      'Dậu',
+      'Tuất',
+      'Hợi',
+      'Tý',
+      'Sửu',
+      'Dần',
+      'Mão',
+      'Thìn',
+      'Tỵ',
+      'Ngọ',
+      'Mùi',
+    ];
+    return '${can[lunarYear % 10]} ${chi[lunarYear % 12]}';
+  }
+
+  static String getNguHanhMenh(int lunarYear) {
+    const canValues = [4, 4, 5, 5, 1, 1, 2, 2, 3, 3];
+    const chiValues = [1, 1, 2, 2, 0, 0, 1, 1, 2, 2, 0, 0];
+    final canIndex = lunarYear % 10;
+    final chiIndex = lunarYear % 12;
+    var menhValue = canValues[canIndex] + chiValues[chiIndex];
+    if (menhValue >= 5) {
+      menhValue -= 5;
+    }
+    return {1: 'Kim', 2: 'Thủy', 3: 'Hỏa', 4: 'Thổ', 5: 'Mộc'}[menhValue]!;
   }
 }
--- lpt/test/util/algorithm/vietnam_calendar_test.dart
+++ lpt/test/util/algorithm/vietnam_calendar_test.dart
@@ -4,18 +4,85 @@ import 'package:lpt/util/calendar/vietnam_calendar.dart';
 void main() {
   group('solar to lunar test', () {
     test('should convert known solar date to lunar correctly', () {
-      final result = VietnamCalendar.convertSolar2Lunar(1, 1, 2024, 7.0);
+      final result = VietnamCalendar.convertSolar2Lunar(1, 1, 2024);
       expect(result, equals((20, 11, 2023, 0)));
     });
 
     test('should convert another solar date to lunar correctly', () {
-      final result = VietnamCalendar.convertSolar2Lunar(10, 5, 2025, 7.0);
+      final result = VietnamCalendar.convertSolar2Lunar(10, 5, 2025);
       expect(result, equals((13, 4, 2025, 0)));
     });
 
     test('should handle leap years properly', () {
-      final result = VietnamCalendar.convertSolar2Lunar(29, 2, 2024, 7.0);
+      final result = VietnamCalendar.convertSolar2Lunar(29, 2, 2024);
       expect(result, equals((20, 1, 2024, 0)));
+    });
+
+    test('should convert known solar date to lunar correctly', () {
+      final result = VietnamCalendar.convertSolar2Lunar(1, 1, 1980);
+      expect(result, equals((14, 11, 1979, 0)));
+    });
+    // Test case : https://phongthuyhomang.vn/cach-tinh-thien-can-dia-chi-va-ngu-hanh-nam-sinh-cuc-nhanh
+    test('Should return correctly', () {
+      expect(VietnamCalendar.getCanChiYear(1995), equalsIgnoringCase('Ất Hợi'));
+      expect(
+        VietnamCalendar.getCanChiYear(2000),
+        equalsIgnoringCase('Canh Thìn'),
+      );
+      expect(
+        VietnamCalendar.getCanChiYear(2004),
+        equalsIgnoringCase('Giáp Thân'),
+      );
+      expect(
+        VietnamCalendar.getCanChiYear(2011),
+        equalsIgnoringCase('Tân Mão'),
+      );
+      expect(
+        VietnamCalendar.getCanChiYear(2016),
+        equalsIgnoringCase('Bính Thân'),
+      );
+      expect(
+        VietnamCalendar.getCanChiYear(2020),
+        equalsIgnoringCase('Canh Tý'),
+      );
+    });
+    test('Should convert correctly', () {
+      expect(
+        VietnamCalendar.getNguHanhMenh(1996),
+        equalsIgnoringCase('Thủy'),
+      ); // Bính Tý
+      expect(
+        VietnamCalendar.getNguHanhMenh(2000),
+        equalsIgnoringCase('Kim'),
+      ); // Canh Thìn
+      expect(
+        VietnamCalendar.getNguHanhMenh(2023),
+        equalsIgnoringCase('Kim'),
+      ); // Quý Mão
+      expect(
+        VietnamCalendar.getNguHanhMenh(2024),
+        equalsIgnoringCase('Hỏa'),
+      ); // Giáp Thìn
+      expect(
+        VietnamCalendar.getNguHanhMenh(1930),
+        equalsIgnoringCase('Thổ'),
+      ); // Canh Ngọ
+      expect(
+        VietnamCalendar.getNguHanhMenh(1931),
+        equalsIgnoringCase('Thổ'),
+      ); // Tân Mùi
+      expect(
+        VietnamCalendar.getNguHanhMenh(1932),
+        equalsIgnoringCase('Kim'),
+      ); // Nhâm Thân
+      expect(
+        VietnamCalendar.getNguHanhMenh(1936),
+        equalsIgnoringCase('Thủy'),
+      ); // Bính Tý
+      expect(
+        VietnamCalendar.getNguHanhMenh(1938),
+        equalsIgnoringCase('Thổ'),
+      ); // Mậu Dần
     });
   });
 }
